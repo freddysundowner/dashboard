8ff6a533a80da4e49e68677bff4c58378eb06d69
PeterLojiKim <pkironji@yahoo.com>
2023-04-03 17:26:02 +0300

    updated home page design

--- lib/details_page.dart
+++ lib/details_page.dart
@@ -10,69 +10,90 @@ class DetailsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-            onPressed: () {
-              Get.back();
-            },
-            icon: Icon(
-              Icons.arrow_back,
-              color: Colors.black,
-            )),
-        backgroundColor: Colors.white,
-        elevation: 0.0,
-        title: Text(
-          "${title}",
-          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
-        ),
-      ),
-      body: ListView.builder(
-          shrinkWrap: true,
-          itemCount: 20,
-          itemBuilder: (context, index) {
-            return Container(
-              padding: EdgeInsets.all(1),
-              margin: EdgeInsets.symmetric(vertical: 5, horizontal: 8),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(height: 15),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: InkWell(
+                    onTap: () {
+                      Get.back();
+                    },
+                    child: Icon(
+                      Icons.arrow_back,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                Spacer(),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("New York Flagship Location"),
+                    Text("1 New York Avenue, New York, NY 11111"),
+                  ],
+                ),
+                Spacer(),
+              ],
+            ),
+            SizedBox(height: 30),
+            Expanded(
+              child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: 20,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      padding: EdgeInsets.all(1),
+                      margin: EdgeInsets.symmetric(vertical: 5, horizontal: 8),
+                      child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text(
-                            "Payroll",
-                            style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.w500),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                DateFormat("dd/MM/yyyy").format(DateTime.now()),
+                                style: TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.w400),
+                              ),
+                              SizedBox(
+                                width: 30,
+                              ),
+                              Flexible(
+                                child: Center(
+                                  child: Text(
+                                    "Payroll for yesterday transactions and today transactions hfyhjugjkhi",
+                                    softWrap: true,
+                                    style: TextStyle(
+                                        letterSpacing: 1,
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w500),
+                                  ),
+                                ),
+                              ),
+                              Text(
+                                "\$ 200.00",
+                                style: TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.w500),
+                              ),
+                            ],
                           ),
-                          SizedBox(height: 5),
-                          Text(
-                            DateFormat("dd/MM/yyyy hh:mm a")
-                                .format(DateTime.now()),
-                            style: TextStyle(
-                                fontSize: 14, fontWeight: FontWeight.w400),
-                          ),
-                          SizedBox(height: 5),
+                          Divider(
+                            thickness: 0.2,
+                            color: Colors.grey,
+                          )
                         ],
                       ),
-                      Text(
-                        "\$ 200",
-                        style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.w500),
-                      ),
-                    ],
-                  ),
-                  Divider(
-                    thickness: 1,
-                    color: Colors.grey,
-                  )
-                ],
-              ),
-            );
-          }),
+                    );
+                  }),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/grid_dashboard.dart
+++ lib/grid_dashboard.dart
@@ -5,124 +5,154 @@ import 'package:get/get.dart';
 class GridDashboard extends StatelessWidget {
   Items item1 = new Items(
       title: "Investment",
-      subtitle: "Sanlam",
-      event: "\$ 20",
+      subtitle: "All money invested by the partner",
+      event: "\$ 20.00",
       color: Color(0XFF97d1c8));
 
   Items item2 = new Items(
       title: "Expenses",
-      subtitle: "Electric Bills",
-      event: "\$ 70",
+      subtitle: "All paid expenses",
+      event: "\$ 70.00",
       color: Color(0XFF212e68));
   Items item3 = new Items(
       title: "Income",
-      subtitle: "Leasing",
-      event: "\$ 120",
+      subtitle: "All received and recorded income",
+      event: "\$ 120.00",
       color: Colors.amber.withOpacity(0.6));
   Items item4 = new Items(
       title: "Outstanding payables",
-      subtitle: "Farm Daily",
-      event: "\$ 300",
+      subtitle: "All unpaid payables",
+      event: "\$ 300.00",
       color: Colors.black.withOpacity(0.6));
   Items item5 = new Items(
       title: "Outstanding liabilities",
-      subtitle: "Loan ",
-      event: "\$ 100",
+      subtitle: "All client billed account receivable",
+      event: "\$ 100.00",
       color: Colors.grey.withOpacity(0.6));
   Items item6 = new Items(
       title: " Current account balance",
-      subtitle: "Saving Account",
-      event: "\$ 500",
+      subtitle: "Outstanding estimated account receivable",
+      event: "\$ 500.00",
       color: Colors.purpleAccent.withOpacity(0.6));
+
+  Items item7 = new Items(
+      title: "Current partners available payout",
+      subtitle: "Maximum amount the partner can currently request",
+      event: "\$ 400.00",
+      color: Color(0XFFf6e6a8));
 
   @override
   Widget build(BuildContext context) {
-    List<Items> myList = [item1, item2, item3, item4, item5, item6];
+    List<Items> myList = [item1, item2, item3, item4, item5, item6, item7];
 
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        titleSpacing: 5,
-        elevation: 0.0,
-        backgroundColor: Colors.white,
-        title: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
+      body: SafeArea(
+        child: Column(
           children: [
-            Icon(Icons.add, size: 35, color: Color(0XFF97d1c8)),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Medrite".toUpperCase(),
-                  style: TextStyle(
-                      color: Color(0XFF212e68),
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20),
-                ),
-                Container(
-                  margin: EdgeInsets.only(left: 20),
-                  child: const Text(
-                    "partner financial portal",
-                    style: TextStyle(color: Colors.grey, fontSize: 13),
+            SizedBox(height: 15),
+            Text("New York Flagship Location"),
+            Text("1 New York Avenue, New York, NY 11111"),
+            SizedBox(height: 15),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 5.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(Icons.add, size: 35, color: Color(0XFF97d1c8)),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Medrite".toUpperCase(),
+                        style: TextStyle(
+                            color: Color(0XFF212e68),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(left: 20),
+                        child: const Text(
+                          "partner financial portal",
+                          style: TextStyle(color: Colors.grey, fontSize: 13),
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-              ],
-            )
+                  Spacer(),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Color(0XFF212e68), shape: BoxShape.circle),
+                    child: Icon(
+                      Icons.person,
+                      color: Colors.white,
+                      size: 30,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 15),
+            Expanded(
+              child: Container(
+                  margin: EdgeInsets.only(top: 10, left: 20, right: 20),
+                  child: ListView.builder(
+                      itemCount: myList.length,
+                      shrinkWrap: true,
+                      physics:ScrollPhysics(),
+                      itemBuilder: (context, index) {
+                        return InkWell(
+                          onTap: () {
+                            Get.to(() => DetailsPage(
+                                title: "${myList[index].title}".capitalize));
+                          },
+                          child: Container(
+                            margin: EdgeInsets.only(bottom: 20),
+                            padding: EdgeInsets.symmetric(vertical: 20),
+                            decoration: BoxDecoration(
+                              color: myList[index].color,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Text(
+                                  myList[index].title!,
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                SizedBox(height: 8),
+                                Text(
+                                  myList[index].subtitle!,
+                                  style: TextStyle(
+                                    color: Colors.white60,
+                                    fontSize: 13,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                                SizedBox(height: 14),
+                                Text(
+                                  myList[index].event!,
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      })),
+            ),
           ],
         ),
       ),
-      body: Container(
-          margin: EdgeInsets.only(top: 10, left: 20, right: 20),
-          child: ListView.builder(
-              itemCount: myList.length,
-              shrinkWrap: true,
-              itemBuilder: (context, index) {
-                return InkWell(
-                  onTap: () {
-                    Get.to(() => DetailsPage(title:"${myList[index].title}".capitalize));
-                  },
-                  child: Container(
-                    margin: EdgeInsets.only(bottom: 20),
-                    padding: EdgeInsets.symmetric(vertical: 20),
-                    decoration: BoxDecoration(
-                      color: myList[index].color,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text(
-                          myList[index].title!,
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                        SizedBox(height: 8),
-                        Text(
-                          myList[index].subtitle!,
-                          style: TextStyle(
-                            color: Colors.white60,
-                            fontSize: 13,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                        SizedBox(height: 14),
-                        Text(
-                          myList[index].event!,
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 15,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              })),
     );
   }
 }
